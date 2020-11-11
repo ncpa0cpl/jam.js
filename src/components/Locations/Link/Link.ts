ceee1c720ac08194ed4b10ae3c9c906722c83272
ncpa0cpl <szymonb21@gmail.com>
2020-11-11 13:28:46 +0100

    locations open function now needs a component object instead of pathRef from context

--- src/components/Locations/Link/Link.ts
+++ src/components/Locations/Link/Link.ts
@@ -1,25 +1,21 @@
-import { TrackedReference } from "../../../general/trackedRef.js";
 import { VDOMComponent as VDC } from "../../../vdom/index.js";
 import { Structure } from "../../../vdom/types.js";
 import { CONTEXT_PARENT_PATH, CONTEXT_PATH_NAME } from "../constants.js";
 import { Path } from "../Path/path.js";
 import { LinkProps } from "./Link.types.js";
 
-function open(context: TrackedReference, path: string) {
+function open<T extends VDC>(path: string, from: T) {
+  const pathRef = from.getContext(CONTEXT_PATH_NAME);
   window.history.pushState({}, "", path);
-  context.current = path;
+  pathRef.current = path;
 }
 
 class Link extends VDC {
   clickHandler() {
     const currentParentPath = this.getContext(CONTEXT_PARENT_PATH);
-    const pathRef = this.getContext(CONTEXT_PATH_NAME);
-
     const props = <LinkProps>this.getProps();
-
     const linkTo = props.to[0] === "/" ? props.to : Path.join(currentParentPath, props.to);
-
-    open(pathRef, linkTo);
+    open(linkTo, this);
   }
 
   render(props: LinkProps): Structure {
